72d42b7e6192851ee89c8219b95dc78a15a30562
goingenginia <goingenginia-programing@yahoo.co.jp>
2021-08-09 22:39:10 +0900

    3点リーダーとクラスの練習中

---
+++ main.js
@@ -0,0 +1,23 @@
+const baseball=[{
+    num:1,
+    position:"センター",
+    name:"●●",
+},{
+    num:2,
+    position:"ショート",
+    name:"▲▲",
+},{
+    num:3,
+    position:"ファースト",
+    name:"××",
+}
+]
+
+
+class yakyu{
+    constructor(value){
+        console.log(value)
+    }
+}
+
+new yakyu(2)
